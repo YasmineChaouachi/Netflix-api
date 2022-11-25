dd730a904316ee996e5412c2e47311b6582c7c6b
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-11-25 22:02:11 +0100

    add Login API

--- routes/authenController.js
+++ routes/authenController.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
+
 // REGISTER
 router.post("/register", async (req, res) => {
 
@@ -25,5 +27,31 @@ router.post("/register", async (req, res) => {
         res.status(500).send({ message: "user not saved " });
     }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+
+        let data = req.body
+
+        let user = await User.findOne({ email: data.email })
+
+        if (user) {
+            let compare = bcrypt.compareSync(data.password, user.password)
+            if (compare) {
+                let myToken = jwt.sign({ role: user.role, id: user._id }, "SECRET-KEY")
+                res.status(200).send({user, token: myToken })
+            } else {
+                res.status(404).send({ message: "user not found !" })
+            }
+        } else {
+            res.status(404).send({ message: "user not found !" })
+        }
+
+    } catch (error) {
+        res.status(400).send({ message: "error fetching user" })
+    }
+})
+
 
 module.exports = router;
