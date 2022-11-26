c4b210589e1bbdbd0d4f659760a23d917f3887e1
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-11-26 18:17:56 +0100

    Users stat API

--- routes/usersController.js
+++ routes/usersController.js
@@ -60,14 +60,53 @@ router.get("/find/:id", async (req, res) => {
 //GET ALL USERS
 
 router.get("/find/", async (req, res) => {
-        try {
-            let users = await User.find();
-            res.status(200).send(users);
-        } catch (e) {
-            res.status(400).send({ message: "error fetching users" });
-        }
+    try {
+        let users = await User.find();
+        res.status(200).send(users);
+    } catch (e) {
+        res.status(400).send({ message: "error fetching users" });
+    }
 })
 
 //GET USER STATS
+
+router.get("/sats/", async (req, res) => {
+    try {
+        let today = new Date();
+        let lastYear = today.setFullYear(today.setFullYear() - 1);
+
+        let monthsArray = [
+            "janvier",
+            "février",
+            "Mars",
+            "avril",
+            "Mai",
+            "juin",
+            "juillet",
+            "août",
+            "septembre",
+            "octobre",
+            "novembre",
+            "décembr",]
+
+        let data = await User.aggregate([
+            {
+                $project: {
+                    month: { $month: "$createdAt" }
+                }
+            }, {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 }
+                }
+            }
+        ]);
+        res.status(200).json(data)
+
+    } catch (e) {
+        res.status(400).json(e);
+    }
+})
+
 
 module.exports = router;
