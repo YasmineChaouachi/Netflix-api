695a63f4626c43048e66011c041a6e63d6a41820
ChaouachiYasmine <chaouachiyasmine28@gmail.com>
2022-11-26 17:00:25 +0100

    Delete users API

--- routes/usersController.js
+++ routes/usersController.js
@@ -27,24 +27,24 @@ router.patch("/:id", async (req, res) => {
 
 //DELETE
 
-// router.delete("/:id", async (req, res) => {
-//     try {
-//         let userId = req.params.id
+router.delete("/:id", async (req, res) => {
+    try {
+        let userId = req.params.id
 
-//         let user = await User.findOneAndDelete({ _id: userId })
+        let user = await User.findOneAndDelete({ _id: userId })
 
-//         if (user) {
-//             res.status(200).send({ message: "user deleted succesfully" })
-//         }
-//         else {
-//             res.status(404).send({ message: "user not found !" })
-//         }
+        if (user) {
+            res.status(200).send({ message: "user deleted succesfully" })
+        }
+        else {
+            res.status(404).send({ message: "user not found !" })
+        }
 
-//     } catch (error) {
-//         res.status(400).send({ message: "error fetching user" })
-//     }
+    } catch (error) {
+        res.status(400).send({ message: "error fetching user" })
+    }
 
-// });
+});
 
 //GET
 //GET ALL USERS
